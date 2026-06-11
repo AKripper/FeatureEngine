4bc04b2e135379f7e755bec1443b9d012d921435
AKripper <167231621+AKripper@users.noreply.github.com>
2026-06-12 03:10:29 +0530

    Update load_sample_datasets.py

--- load_sample_datasets.py
+++ load_sample_datasets.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent
@@ -6,13 +5,4 @@ BASE_DIR = Path(__file__).parent
 SAMPLE_DATASETS = {
     "Titanic": BASE_DIR / "sample_datasets" / "titanic.csv",
     "Housing": BASE_DIR / "sample_datasets" / "housing.csv",
-=======
-from pathlib import Path
-
-BASE_DIR = Path(__file__).parent
-
-SAMPLE_DATASETS = {
-    "Titanic": BASE_DIR / "sample_datasets" / "titanic.csv",
-    "Housing": BASE_DIR / "sample_datasets" / "housing.csv",
->>>>>>> 5ebcbc9c35363a302ffdbd63fc0dacf8f664a4fc
 }
